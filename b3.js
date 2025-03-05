4491f182b97a72bbc748ab98a7d61a153c27acdf
hahuydz <haquanghuy2006abc@gmail.com>
2025-03-05 21:18:25 +0700

    B3

---
+++ b3.js
@@ -0,0 +1,8 @@
+let password = "12345";
+let inputPassword = prompt("moi ban nhap vao mat khau")
+
+if (inputPassword !== password) {
+    alert(`khong dung mat khau`);
+} else {
+    alert(`dung mat khau`);
+}
